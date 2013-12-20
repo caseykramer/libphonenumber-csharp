f8ee8fc46f246a40ab9b02e182a6fb488bc0a302
Casey Kramer <ckramer@exacttarget.com>
2013-12-20 12:43:49 -0500

    Updates to projects/nuget package files

--- csharp/PhoneNumbers/Properties/AssemblyInfo.cs
+++ csharp/PhoneNumbers/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: InternalsVisibleTo("PhoneNumbers.Test")]
+[assembly: InternalsVisibleTo("PhoneNumbers.Test,PublicKey=0024000004800000940000000602000000240000525341310004000001000100bde2afb5c7e52bae4645cd6f0a17da44b588ece28058a2597d0540627b977c0fbc358bd4a8c3629c498b2c04cf194149c452e6ffa465cb3dd83993326a15e4068f93e135b1c8269041e2e8e9b77c5f3d71709e2cd76e016ba87dd5ff3b1ba7448c0aa8f6367c4f6077a749731fc5fcbe8325161264b5017a13602982ec95fdc6")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("5.1.0.66")]
-[assembly: AssemblyVersion("5.1.0.66")]
-[assembly: AssemblyFileVersion("5.1.0.66")]
+// [assembly: AssemblyVersion("5.1.0.67")]
+[assembly: AssemblyVersion("5.1.0.67")]
+[assembly: AssemblyFileVersion("5.1.0.67")]
