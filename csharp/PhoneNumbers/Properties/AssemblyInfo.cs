6cb27c6726d0d2098fa318dd11ced9298b1d0f0a
Casey Kramer <ckramer@exacttarget.com>
2014-02-11 13:21:16 -0500

    Metadata update

--- csharp/PhoneNumbers/Properties/AssemblyInfo.cs
+++ csharp/PhoneNumbers/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("5.1.0.67")]
-[assembly: AssemblyVersion("5.1.0.67")]
-[assembly: AssemblyFileVersion("5.1.0.67")]
+// [assembly: AssemblyVersion("5.1.0.68")]
+[assembly: AssemblyVersion("5.1.0.68")]
+[assembly: AssemblyFileVersion("5.1.0.68")]
