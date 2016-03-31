2dc6de271fe53abd21050709bff9bcce874d326a
Casey Kramer <ckramer@salesforce.com>
2016-03-31 10:37:27 -0400

    Merge in latest changes from google

--- csharp/PhoneNumbers/Properties/AssemblyInfo.cs
+++ csharp/PhoneNumbers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("Google's libphonenumber")]
 [assembly: AssemblyProductAttribute("PhoneNumbers")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Google 2015")]
-[assembly: AssemblyVersionAttribute("7.2.8")]
-[assembly: AssemblyFileVersionAttribute("7.2.8")]
+[assembly: AssemblyVersionAttribute("7.2.9")]
+[assembly: AssemblyFileVersionAttribute("7.2.9")]
 [assembly: AssemblyKeyFileAttribute("key.snk")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "7.2.8";
+        internal const string Version = "7.2.9";
     }
 }
