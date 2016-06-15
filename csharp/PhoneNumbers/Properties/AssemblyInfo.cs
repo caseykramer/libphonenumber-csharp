8959ac9e1cff5ab6256cc5a1b97893c9850a0574
Casey Kramer <ckramer@salesforce.com>
2016-06-15 13:16:47 -0400

    Metadata updates

--- csharp/PhoneNumbers/Properties/AssemblyInfo.cs
+++ csharp/PhoneNumbers/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Reflection;
 [assembly: AssemblyDescriptionAttribute("Google's libphonenumber")]
 [assembly: AssemblyProductAttribute("PhoneNumbers")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Google 2015")]
-[assembly: AssemblyVersionAttribute("7.4.3")]
-[assembly: AssemblyFileVersionAttribute("7.4.3")]
+[assembly: AssemblyVersionAttribute("7.4.4")]
+[assembly: AssemblyFileVersionAttribute("7.4.4")]
 [assembly: AssemblyKeyFileAttribute("key.snk")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "7.4.3";
+        internal const string Version = "7.4.4";
     }
 }
