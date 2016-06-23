52d0575b530a2418a8fe33eb0bd9f9a88d84ae80
Casey Kramer <ckramer@salesforce.com>
2016-06-23 15:38:16 -0400

    Actually have to generate the new yaml file for appveyor to use it

--- csharp/PhoneNumbers/Properties/AssemblyInfo.cs
+++ csharp/PhoneNumbers/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("PhoneNumbers")]
 [assembly: AssemblyDescriptionAttribute("Google's libphonenumber")]
 [assembly: AssemblyProductAttribute("PhoneNumbers")]
-[assembly: AssemblyCopyrightAttribute("Copyright © Google 2015-2016")]
-[assembly: AssemblyVersionAttribute("7.4.4.5")]
-[assembly: AssemblyFileVersionAttribute("7.4.4.5")]
+[assembly: AssemblyCopyrightAttribute("Copyright © 2015-2016 Google")]
+[assembly: AssemblyVersionAttribute("7.4.4.6")]
+[assembly: AssemblyFileVersionAttribute("7.4.4.6")]
 [assembly: AssemblyKeyFileAttribute("key.snk")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "7.4.4.5";
+        internal const string Version = "7.4.4.6";
     }
 }
